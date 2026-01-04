24292f2b0af9e6ec4b0efe9e1ff50deb7f65f122
ram <ramshu3000@gmail.com>
2026-01-04 22:45:12 +0530

    demo

--- tasks/task1/Demo.java
+++ tasks/task1/Demo.java
@@ -1,11 +1,13 @@
 public class Demo {
     static int a=10;
+    static int b=20;
     static  void add(){
-        System.out.println(a+a);
+        System.out.println(a+b);
     }
     public static void main(String[] args) {
         add();
         System.out.println(a);
+        System.out.println(b);
     }
 }
  
