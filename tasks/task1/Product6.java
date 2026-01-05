16e15088a96ae092968561cb9c42d313ddb2aa85
ram <ramshu3000@gmail.com>
2026-01-05 18:13:37 +0530

    sc_product6_upadted

--- tasks/task1/Product6.java
+++ tasks/task1/Product6.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 public class Product6 {
     
     String name;
     double price;
+
     Product6(String name, double price) {
         this.name = name;
         this.price = price;
@@ -12,7 +14,17 @@ public class Product6 {
     }
 
     public static void main(String[] args) {
-        Product6 product = new Product6("Laptop", 60000);
+
+        
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter Product Name");
+        String name=sc.nextLine();
+        System.out.println("Enter Price");
+        double Price=sc.nextDouble();
+                                    // Product6 product = new Product6("Laptop", 60000);
+        Product6 product = new Product6(name, Price);
+                                                    
 
         System.out.println("Original Price: " + product.price);
 
