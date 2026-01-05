16497a6928d4c02f1123508e2258b2e91a5cd228
ram <ramshu3000@gmail.com>
2026-01-05 11:47:24 +0530

    car1_upadted

--- tasks/task1/Car1.java
+++ tasks/task1/Car1.java
@@ -4,19 +4,29 @@ public class Car1 {
     String brand;
     String model;
     double price;
-    
-   void getDetails() {
-        Scanner sc1 = new Scanner(System.in);
 
-        System.out.print("Enter Car Brand: ");
-        brand = sc1.nextLine();
-
-        System.out.print("Enter Car Model: ");
-        model = sc1.nextLine();
-
-        System.out.print("Enter Car Price: ");
-        price = sc1.nextDouble();
+    public Car1(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
     }
+
+    
+
+    
+    
+    // void getDetails() {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter Car Brand: ");
+    //     brand = sc.nextLine();
+
+    //     System.out.print("Enter Car Model: ");
+    //     model = sc.nextLine();
+
+    //     System.out.print("Enter Car Price: ");
+    //     price = sc.nextDouble();
+    // }
     void displayDetials(){
         System.out.println("\n--- Car Details ---");
         System.out.println("Brand : " + brand);
@@ -25,10 +35,20 @@ public class Car1 {
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Car Brand: ");
+      String  brand = sc.nextLine();
+
+        System.out.print("Enter Car Model: ");
+       String model = sc.nextLine();
+
+        System.out.print("Enter Car Price: ");
+      double  price = sc.nextDouble();
                                 //new Car().getDetails();
                                 //new Car().displayDetials();
-        Car1 c1=new Car1();
-        c1.getDetails();
+        Car1 c1=new Car1(brand, model,price);
+        // c1.getDetails();
         c1.displayDetials();
     }
 }
